1faad14b5f96ff0ef5de59ffe0087124ee50d1a2
Colton Kammes <ckammes@nd.edu>
2022-04-14 14:57:04 -0400

    fixed solver and created a class for use as a library- easy to incorporate in quick flask website

---
+++ semantleClass.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+import gensim
+
+class SemantleSolver:
+    def __init__(self, error=.01, wordsfile='words.pkl', vecsFile='vecs.pkl', pruneWords=True):
+        self.error = error
+        self.wordsfile = wordsfile
+        self.vecsFile = vecsFile
+        self.pruneWords = pruneWords
+
+        self.candidates = []
+        self.words = []
+        self.vecs = None
+
+    def loadVec(self):
+        #  print("Loading vectors...")
+        self.vecs = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+        #  print("Vectors loaded!")
+
+    def loadWords(self):
+        if not self.vecs:
+            self.loadVec()
+
+        words = [""] * 3000000
+        for i in range(3000000):
+            words[i] = (self.vecs.index_to_key[i])
+
+        if self.pruneWords:
+            #  print("Pruning...")
+            actualWords = []
+            for word in words:
+                if word.isalnum() and word.islower():
+                    actualWords.append(word)
+            words = actualWords
+            #  print("Done pruning!")
+        self.words = words
+
+    def getCandidates(self, k):
+        return self.candidates[:k]
+
+    def inputGuess(self, guess, score):
+        #  print("Searching...")
+        candidates = []
+        filteredWords = []
+        score = float(score)/100
+
+        for word in self.words:
+            potentialScore = self.vecs.similarity(word, guess)
+            if abs(score - potentialScore) <= self.error:
+                if word.isalnum() and word.islower():
+                    candidates.append((word, score-potentialScore))
+                    filteredWords.append(word)
+
+        candidates.sort(key=lambda y: abs(y[1]), reverse=False)
+
+        self.words = filteredWords
+        self.candidates = candidates
+
+
+def main():
+    s = SemantleSolver()
+    s.loadWords()
+
+    while True:
+        word = input("Entered word: ")
+        score = input("Enter its score: ")
+
+        s.inputGuess(word, score)
+
+        possibilities = s.getCandidates(5)
+        print("Guesses:")
+        for i, possib in enumerate(possibilities):
+            print(f'{i+1}: {possib[0]} {possib[1]}')
+
+if __name__ == "__main__":
+    main()
+
--- semantleSolver.py
+++ semantleSolver.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
 import gensim
+import pickle
+
+wordsfile = "words.pkl"
+vecsFile = 'vecs.pkl'
 
 error = .01
+numCandidates = 10
 
 def loadVec():
     print("Loading vectors...")
@@ -13,21 +18,34 @@ def loadVec():
 def getCandidates(guess, score, words, vecs):
     print("Searching...")
     candidates = []
+    filteredWords = []
     score = float(score)/100
 
     for word in words:
-        #  print(f'word: {word}\tguess: {guess}')
+        '''
+        say guess is beer - 11.7
+        check beer vs all other words
+            if the similarity is close to 11.7, then it should be a part of the ring around the main word
+        '''
         potentialScore = vecs.similarity(word, guess)
         if abs(score - potentialScore) <= error:
             if word.isalnum() and word.islower():
                 candidates.append((word, score-potentialScore))
+                filteredWords.append(word)
 
     candidates.sort(key=lambda y: abs(y[1]), reverse=False)
 
-    if len(candidates) > 5:
-        candidates = candidates[:5]
+    if len(candidates) > numCandidates:
+        candidates = candidates[:numCandidates]
 
-    return candidates
+    return filteredWords, candidates
+
+'''
+returns up to k number of guesses based on the most recent guess and score
+'''
+def getKGuesses(guess, score, k):
+    pass
+
 
 def main():
     vecs = loadVec()
@@ -35,14 +53,23 @@ def main():
     for i in range(3000000):
         words[i] = (vecs.index_to_key[i])
 
+    print("Pruning...")
+    actualWords = []
+    for word in words:
+        if word.isalnum() and word.islower():
+            actualWords.append(word)
+    words = actualWords
+    print("Done pruning!")
+        
+
     while True:
         word = input("Entered word: ")
         score = input("Enter its score: ")
 
-        possibilities = getCandidates(word, score, words, vecs)
+        words, possibilities = getCandidates(word, score, words, vecs)
         print("Guesses:")
         for i, possib in enumerate(possibilities):
-            print(f'{i}: {possib}')
+            print(f'{i+1}: {possib[0]} {possib[1]}')
 
 if __name__ == "__main__":
     main()
